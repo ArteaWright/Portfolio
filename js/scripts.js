a5b7daaeb9c53010732ce51db4ab3c87b8895b4e
MrsSprinkle <artealwright@gmail.com>
2022-06-24 14:28:37 -0400

    Add images, desktop styling, and scripting

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+// References project elements 
+
+const react = document.querySelector(".react");
+const angular = document.querySelector(".angular"); 
+const node = document.querySelector(".node");
+const api = document.querySelector(".api");
+
+// Handles click event for each project
+
+react.addEventListener("click", () => {
+    window.location.href = "https://www.canva.com/"; 
+})
+
+angular.addEventListener("click", () => {
+    window.location.href = "https://www.google.com/";
+})
+
+node.addEventListener("click", ()=> {
+    window.location.href = "https://bing.com/";
+})
+
+api.addEventListener("click", ()=> {
+    window.location.href = "https://w3schools.com/";
+})
+
+
